18a086b778414eef5bc5f0efee96a8e5ac423825
rongwei <rongwei20@foxmail.com>
2022-05-11 01:54:53 +0800

    修改博客列表页码

--- src/components/admin/BlogIInofDetail.vue
+++ src/components/admin/BlogIInofDetail.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="allBlogs">
+    <!--筛选表单-->
     <el-form class="form" inline="true">
       <el-row  :gutter="20">
         <el-col :span="6">
@@ -33,10 +34,48 @@
         </el-col>
         <el-col :span="6">
           <el-button type="primary">查询</el-button>
-          <el-button >重置</el-button>
+          <el-button>重置</el-button>
+          <el-button type="success">新增</el-button>
         </el-col>
       </el-row>
     </el-form>
+
+    <!--博客列表-->
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        label="标题"
+        width="180">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="分类"
+        width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
 
   </div>
@@ -64,8 +103,38 @@ export default {
         label: '北京烤鸭'
       }],
       value1: [],
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
-  }
+  },
+
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+
+
+
+
+    }
 }
 </script>
 
